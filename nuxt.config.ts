5f53636a5113e527729bcfc0d2c792fd845edcc5
ICT Layer <32593898+ICTLAYER@users.noreply.github.com>
2024-12-08 01:21:35 +0600

    update for deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,39 +7,9 @@ export default defineNuxtConfig({
     '@nuxt/icon',
     '@nuxt/image',
     '@vueuse/nuxt',
-    '@nuxtjs/seo',
-    '@nuxthub/core'
+    '@nuxthub/core',
+    '@nuxt/scripts'
   ],
-  site: {
-    url: 'https://www.madewithnuxtjs.com',
-    name: 'Made With Nuxtjs',
-    canonicalQueryWhitelist: ['myCustomQuery'],
-    redirectToCanonicalSiteUrl: true
-  },
-  seo: {
-    meta: {
-      description: 'Made With Nuxtjs',
-      themeColor: [
-        { content: '#18181b', media: '(prefers-color-scheme: dark)' },
-        { content: 'white', media: '(prefers-color-scheme: light)' },
-      ],
-      twitterCreator: '@ictlayer',
-      twitterSite: '@ictlayer',
-      author: 'ICT Layer',
-      colorScheme: 'dark light',
-      applicationName: 'Made With Nuxtjs',
-      // Nuxt SEO Utils already sets the below tags for you
-      ogSiteName: 'Made With Nuxtjs',
-      ogLocale: 'en_US',
-      ogType: 'website',
-      // ogUrl: 'https://www.madewithnuxtjs.com',
-      ogTitle: 'Made With Nuxtjs',
-
-      // Other Nuxt SEO modules handles these
-      // ogImage: 'https://www.madewithnuxtjs.com/logo.png',
-      robots: 'index, follow',
-    }
-  },
   image: {
     quality: 80,
   },
